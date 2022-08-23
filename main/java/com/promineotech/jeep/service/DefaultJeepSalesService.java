1d931d4e801c2f8e1476705a4933113391da7230
cel90 <86092818+cel90@users.noreply.github.com>
2022-08-22 20:30:16 -0400

    first commit

--- main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java
+++ main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java
@@ -1,5 +1,6 @@
 package com.promineotech.jeep.service;
 
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,13 @@ public class DefaultJeepSalesService implements JeepSalesService {
  @Autowired
  private JeepSalesDao jeepSalesDao ;
  
+ @Override
   public List<Jeep> fetchJeeps(JeepModel model, String trim) {
     // TODO Auto-generated method stub
     log.info("fetchJeeps method was called with model={},trim={}", model, trim);
  List<Jeep> jeeps = jeepSalesDao.fetchJeeps(model, trim) ;
  
+
   return jeeps ;
 }
 }
